20e1f4b0b3e81a17fed61f94185215d4c2068d52
aleksifuna <aleksifuna@gmail.com>
2024-01-28 18:18:19 +0000

    added a template and flask file for task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module supplies a script that starts a flask web application
+"""
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+objs = storage.all(State)
+
+@app.route('/states_list', strict_slashes=False)
+def state_list(n):
+    """
+    Displays a list of states in the db
+    """
+    return render_template('7-number_odd_or_even.html', objs = objs)
+
+@app.teardown_appcontext
+def teardown_db(exeption):
+    """
+    removes the current session after each request
+    """
+    storage.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
